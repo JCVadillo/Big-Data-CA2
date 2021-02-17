2e5dec5d182e73002b4918addaf2a015b8c62156
JCVadillo <43447117+JCVadillo@users.noreply.github.com>
2021-02-17 14:50:55 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+/*This the server*/
+
+require('./db/db-connection.js')
+const express = require('express')
+const router = require('./routes')
+
+const app = express()
+
+//Heroku's enviorment port or local port 3000
+const port = process.env.PORT || 3000
+app.use(express.static('public'));
+app.use(express.json())
+app.use(router)
+
+app.listen(port);
+
 
 //Variable to fill up wit json data
 Beers = [];
